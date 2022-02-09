1b86ac3cb5e38e1f3fa0610566693e25f09ccbb1
Awlex <no thank you>
2022-02-09 02:46:12 +0100

    Use pipe

--- lib/hexerss/hexpm/cache.ex
+++ lib/hexerss/hexpm/cache.ex
@@ -2,9 +2,11 @@ defmodule Hexerss.Hexpm.Cache do
   def child_spec(opts) do
     config = Application.get_env(:hexerss, __MODULE__)
 
-    opts = Keyword.put_new(opts, :name, __MODULE__)
-    opts = Keyword.put_new(opts, :global_ttl, config[:global_ttl])
-    opts = Keyword.put_new(opts, :ttl_check_interval, config[:ttl_check_interval])
+    opts =
+      opts
+      |> Keyword.put_new(:name, __MODULE__)
+      |> Keyword.put_new(:global_ttl, config[:global_ttl])
+      |> Keyword.put_new(:ttl_check_interval, config[:ttl_check_interval])
 
     Supervisor.child_spec({ConCache, opts}, id: __MODULE__)
   end

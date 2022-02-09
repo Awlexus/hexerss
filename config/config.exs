cbd26fd15ab2a500683e6f2ec82543456a4b6d4a
Awlex <no thank you>
2022-02-09 23:30:53 +0100

    recompile when html changes

--- config/config.exs
+++ config/config.exs
@@ -5,3 +5,5 @@ config :tesla, adapter: Tesla.Adapter.Hackney
 config :hexerss, Hexerss.Hexpm.Cache,
   ttl_check_interval: :timer.minutes(5),
   global_ttl: :timer.minutes(30)
+
+config :exsync, addition_dirs: ~w"/priv", extensions: ~w".ex .html"
